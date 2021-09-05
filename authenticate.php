6dbd75d933b8146223a8167174a86aff9e036c57
theblankone <79501333+theblankone@users.noreply.github.com>
2021-09-05 15:02:50 +0300

    Update authenticate.php
    v

--- authenticate.php
+++ authenticate.php
@@ -11,3 +11,8 @@ if ( mysqli_connect_errno() ) {
 	// If there is an error with the connection, stop the script and display the error.
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
+// Now we check if the data from the login form was submitted, isset() will check if the data exists.
+if ( !isset($_POST['username'], $_POST['password']) ) {
+	// Could not get the data that should have been sent.
+	exit('Please fill both the username and password fields!');
+}
