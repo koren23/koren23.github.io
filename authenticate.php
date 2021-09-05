39f1bb0c00b1e879adf480f1164c63d2c30d339d
theblankone <79501333+theblankone@users.noreply.github.com>
2021-09-05 15:03:06 +0300

    Update authenticate.php
    b

--- authenticate.php
+++ authenticate.php
@@ -16,3 +16,15 @@ if ( !isset($_POST['username'], $_POST['password']) ) {
 	// Could not get the data that should have been sent.
 	exit('Please fill both the username and password fields!');
 }
+// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
+if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+	$stmt->bind_param('s', $_POST['username']);
+	$stmt->execute();
+	// Store the result so we can check if the account exists in the database.
+	$stmt->store_result();
+
+
+	$stmt->close();
+}
+?>
